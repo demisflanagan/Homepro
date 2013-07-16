a7fb8669fd3e89de960d58a83435638b0636c168
demisflanagan <email@demisflanagan.com>
2013-07-16 14:05:12 -0700

    modified template.

--- templates/default/pages/home/blurb.tpl.php
+++ templates/default/pages/home/blurb.tpl.php
@@ -1,8 +1,8 @@
 <?php
 
-	if ($vars['offset'] == 0) {
-		$user = $this->getInput('homepro_username');
-//    $user = \Idno\Entities\User::getByHandle('demisflanagan');
-    $t = $this;
-    echo $t->__(['user' => $user])->draw('entity/User');
-	}
+if ($vars['offset'] == 0) {
+	// $user = <?=htmlspecialchars(\Idno\Core\site()->config()->homepro['homepro_username'])?>
+	$user = \Idno\Entities\User::getByHandle('demisflanagan');
+	$t = $this;
+	echo $t->__(['user' => $user])->draw('entity/User');
+}
