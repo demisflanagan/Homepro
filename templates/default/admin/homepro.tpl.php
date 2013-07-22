76c5dbcad8ef53e4eaea3d9aa0e87a6ff06bee2e
demisflanagan <email@demisflanagan.com>
2013-07-22 12:09:12 -0700

    Modified homepro.tpl.php

--- templates/default/admin/homepro.tpl.php
+++ templates/default/admin/homepro.tpl.php
@@ -10,8 +10,8 @@
     <div class="span10 offset1">
         <form action="/admin/homepro/" class="form-horizontal" method="post">
             <div class="control-group">
-		<p> Enter the username of the profile you would like to appear on the homepage.</p>
-		<p> Invalid usernames it will be ignored and not displayed.               
+		<p>Enter the username of the profile you would like to appear on the homepage.</p>
+		<p><em>Invalid usernames it will be ignored and not displayed.</em></p>
 		<label class="control-label" for="name">Homepro Username</label>
                 <div class="controls">
                     <input type="text" id="name" placeholder="Homepro Username" class="span4" name="homepro_username" value="<?=htmlspecialchars(\Idno\Core\site()->config()->homepro['homepro_username'])?>" >
