acb581134c21626bf043151a95b1c788a1fec73e
root <root@demisflanagan.com>
2013-07-22 11:58:35 -0700

    Modified blurb.tpl.php to fetch and validate username saved in admin->Homepro tab.

--- templates/default/pages/home/blurb.tpl.php
+++ templates/default/pages/home/blurb.tpl.php
@@ -1,7 +1,14 @@
 <?php
 
 if ($vars['offset'] == 0) {
-   $user = \Idno\Entities\User::getByHandle('demisflanagan');
-   $t = $this;
-   echo $t->__(['user' => $user])->draw('entity/User');
+   
+   // Check if username exists in the Admin->Homeplug tab and get it:
+   if ($username = htmlspecialchars(\Idno\Core\site()->config()->homepro['homepro_username'])) {
+      
+      //check if username is valid or not:
+      if ($user = \Idno\Entities\User::getByHandle( $username )) {
+         $t = $this;
+         echo $t->__(['user' => $user])->draw('entity/User');
+      }
+   }
 }
